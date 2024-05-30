db47fcbb425a3d68b2603c4b19c2962df65c89dd
Sergii Kirianov <s.kirianov@gmail.com>
2024-05-30 12:26:52 -0600

    complete product page

--- storefront-react-native/app/Product Details/[product_code]/index.tsx
+++ storefront-react-native/app/Product Details/[product_code]/index.tsx
@@ -2,39 +2,97 @@ import { Text, View } from "@/components/Themed";
 import { sdk } from "@/sdk/sdk.config";
 import { transformImageUrl } from "@/utils/transformImage";
 import { Product } from "@vsf-enterprise/sap-commerce-webservices-sdk";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams } from "expo-router"
 import { useEffect, useState } from "react";
-import { Image } from "react-native";
+import { Dimensions, Image, ScrollView, StyleSheet } from "react-native";
 import Carousel from "react-native-reanimated-carousel";
 
-export default function ProductDetails() {
+export default function ProductScreen() {
   const { product_code } = useLocalSearchParams();
-  const [product, setProduct] = useState<Product | undefined>(undefined);
+  const [product, setProduct] = useState<Product | null>(null);
+  const width = Dimensions.get("window").width;
 
   useEffect(() => {
-    async function getProduct() {
-      const product = await sdk.sapcc.getProduct({
-        id: product_code as string,
-      });
+    async function fetchProduct() {
+      const data = await sdk.sapcc.getProduct({ id: product_code as string });
 
-      setProduct(product);
+      setProduct(data);
     }
 
-    getProduct();
-  }, [])
+    fetchProduct();
+  }, []);
+
+  if (!product) {
+    return <Text>Loading...</Text>;
+  }
+
+  const galleryImages = product?.images?.filter((image) => image.imageType === "GALLERY" && image.format === "product")
+    .map((image) => transformImageUrl(image.url as string)) as [] | string[];
 
   return (
-    <View>
-      <Text>Product Details</Text>
-      <Carousel
-        data={product?.images?.map((image) => transformImageUrl(image.url as string)) ?? []}
-        width={300}
-        renderItem={({ item }) => (
-          <View style={{ width: 300, height: 300 }}>
-            <Image source={{ uri: item }} style={{ width: 300, height: 300 }} />
-          </View>
-        )}
-      />
-    </View>
+    <ScrollView style={styles.page}>
+      <View style={{
+        ...styles.container,
+      }}>
+        <View>
+          <Carousel
+            loop
+            pagingEnabled
+            snapEnabled
+            style={styles.imageContainer}
+            width={width * 0.95}
+            height={width * 0.8}
+            data={galleryImages}
+            renderItem={({ item }) => (
+              <Image
+                source={{ uri: item }}
+                style={{ width: width * 0.95, height: width * 0.8 }}
+              />
+            )}
+          />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.textBold}>{product.name}</Text>
+          <Text style={styles.textBold}>{product.price?.formattedValue}</Text>
+        </View>
+        <View style={styles.summaryContainer}>
+          <Text style={styles.summaryText}>{product?.summary}</Text>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "#fff",
+    flex: 1,
+    padding: 20,
+  },
+  container: {
+    flex: 1,
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textContainer: {
+    flex: 1,
+    flexDirection: "column",
+    gap: 10,
+    marginTop: 20,
+  },
+  textBold: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  summaryContainer: {
+    marginTop: 16,
+  },
+  summaryText: {
+    fontSize: 16,
+    lineHeight: 20,
+    color: "#666",
+  },
+})
